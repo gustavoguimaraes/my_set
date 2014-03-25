8356730c5badab0eee3eb7aed56f3b465ec7e30f
Gustavo <gustavoguimaraes@gmail.com>
2014-03-25 18:44:17 -0400

    First Commit

---
+++ set.rb
@@ -0,0 +1,41 @@
+class MySet 
+  attr_accessor :counter, :elements
+  def initialize
+    @counter = 0
+    @elements = []
+  end
+
+  def empty?
+    if @counter == 0
+      true
+    else
+      false
+    end
+  end
+
+  def add(data)
+    unless include?(data)
+      @elements[@counter] = data
+      @counter += 1
+    else
+      raise CannotAddSameData
+    end
+  end
+
+  def size
+    @counter
+  end
+
+  def include?(data)
+    @elements.each do |elem| 
+      if elem == data
+        return true
+      end
+    end
+    return false
+  end
+
+end
+
+class CannotAddSameData < StandardError
+end
